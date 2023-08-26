99ae456c8110f397af7f410831445a23dc061cd9
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2023-08-26 15:48:33 +0300

    26.08.2023

--- src/test/java/pages/Pages.java
+++ src/test/java/pages/Pages.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -16,5 +18,20 @@ public class Pages {
     public WebElement getKampanyaPopup() {
         return kampanyaPopup;
     }
+    @FindBy(css = "div.desktop > div > div.banner__accept-button")
+    public WebElement kabulEt;
+/*
+    public WebElement getShadowRootElement(WebElement element) {
+        WebElement ele = (WebElement) ((JavascriptExecutor)Driver.getDriver())
+                .executeScript("return arguments[0].shadowRoot", element);
+        return ele;
+    }
 
-}
+    WebElement root1=Driver.getDriver().findElement(By.className("efilli-layout-n11"));
+    WebElement shadowRoot1=getShadowRootElement(root1);
+    WebElement shadowElement=shadowRoot1.findElement(By.cssSelector("div[class=banner__accept-button"));
+
+
+    //document.querySelector("body > efilli-layout-n11").shadowRoot.querySelector("div.desktop > div > div.banner__accept-button")
+
+*/}
--- src/test/java/tests/N11_Tests.java
+++ src/test/java/tests/N11_Tests.java
@@ -1,5 +1,10 @@
 package tests;
 
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import pages.Pages;
 import utils.ConfigReader;
@@ -12,12 +17,16 @@ public class N11_Tests {
     Pages elements = new Pages();
 
     @Test
-    public void AnasayfayaGidis(){
+    public void AnasayfayaGidis() {
         Driver.getDriver().get("https://www.n11.com/");
-        assertEquals(Driver.getDriver().getTitle(),"n11 - 10 Üzerinden 11'lik Alışveriş Deneyimi");
+        assertEquals(Driver.getDriver().getTitle(), "n11 - 10 Üzerinden 11'lik Alışveriş Deneyimi");
         elements.getKampanyaPopup().click();
+
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveByOffset(1342,20).click().perform();
 
 
     }
+
 
 }
